7d2869474cd95ead7d0714827b4595eed5ed20ad
Adrian Karbowniczyn <adrian.karbowniczyn@gmail.com>
2020-02-15 08:06:08 +0100

    Add input validation to ROT13

--- js/caesar13.js
+++ js/caesar13.js
@@ -1,3 +1,12 @@
-const caesar13 = text => {};
+import ValidationError from './ValidationError';
+
+const caesar13 = text => {
+  if (typeof text !== 'string') {
+    throw new ValidationError('Input is not a string!');
+  }
+  if (text.length === 0 || !text.trim()) {
+    throw new ValidationError('Input is empty!');
+  }
+};
 
 export default caesar13;
