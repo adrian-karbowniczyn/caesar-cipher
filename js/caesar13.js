bdf97c64a95b9b85e82a05bf085a87903459e2be
Adrian Karbowniczyn <adrian.karbowniczyn@gmail.com>
2020-02-15 08:23:57 +0100

    Refactor ROT13 algorithm

--- js/caesar13.js
+++ js/caesar13.js
@@ -17,18 +17,23 @@ const caesar13 = text => {
   }
   const characters = [...text];
   const cipherROT13 = [];
+  const BEGIN_UPPERCASE = 65;
+  const BEGIN_LOWERCASE = 97;
+  const END_UPPERCASE = 90;
+  const END_LOWERCASE = 122;
 
   for (let i = 0; i < characters.length; i++) {
     const currentASCII = characters[i].charCodeAt(0);
 
-    if (currentASCII >= 65 && currentASCII <= 90) {
-      cipherROT13.push(String.fromCharCode(((currentASCII + 13 - 65) % 26) + 65));
-    } else if (currentASCII >= 97 && currentASCII <= 122) {
-      cipherROT13.push(String.fromCharCode(((currentASCII + 13 - 97) % 26) + 97));
+    if (isInBetween(BEGIN_UPPERCASE, currentASCII, END_UPPERCASE)) {
+      cipherROT13.push(String.fromCharCode(swapASCII(BEGIN_UPPERCASE, currentASCII)));
+    } else if (isInBetween(BEGIN_LOWERCASE, currentASCII, END_LOWERCASE)) {
+      cipherROT13.push(String.fromCharCode(swapASCII(BEGIN_LOWERCASE, currentASCII)));
     } else {
       cipherROT13.push(String.fromCharCode(currentASCII));
     }
   }
+
   return cipherROT13.join('');
 };
 
