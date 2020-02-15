0942d796d0fe7d90f3591440add2983b513808a4
Adrian Karbowniczyn <adrian.karbowniczyn@gmail.com>
2020-02-15 08:19:01 +0100

    Write ROT13 algorithm

--- js/caesar13.js
+++ js/caesar13.js
@@ -7,6 +7,21 @@ const caesar13 = text => {
   if (text.length === 0 || !text.trim()) {
     throw new ValidationError('Input is empty!');
   }
+  const characters = [...text];
+  const cipherROT13 = [];
+
+  for (let i = 0; i < characters.length; i++) {
+    const currentASCII = characters[i].charCodeAt(0);
+
+    if (currentASCII >= 65 && currentASCII <= 90) {
+      cipherROT13.push(String.fromCharCode(((currentASCII + 13 - 65) % 26) + 65));
+    } else if (currentASCII >= 97 && currentASCII <= 122) {
+      cipherROT13.push(String.fromCharCode(((currentASCII + 13 - 97) % 26) + 97));
+    } else {
+      cipherROT13.push(String.fromCharCode(currentASCII));
+    }
+  }
+  return cipherROT13.join('');
 };
 
 export default caesar13;
