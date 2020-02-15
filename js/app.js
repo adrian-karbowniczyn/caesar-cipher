6734fc1be4daf17f2c88375bc6a8d27894bb6108
Adrian Karbowniczyn <adrian.karbowniczyn@gmail.com>
2020-02-15 08:52:18 +0100

    Add consts to store a refs to the DOM elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 import caesar13 from './caesar13';
+
+const submitButton = document.querySelector('.cryptography-form__submit');
+const textInput = document.querySelector('.cryptography-form__input');
+const outputMessage = document.querySelector('.cryptography-output__encrypted-message');
+const errorMessage = document.querySelector('.cryptography-output__error-message');
